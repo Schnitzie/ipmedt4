f911cfba543e28fd6d132fb3fb0bb398278ac50e
Jan <Jan@Jan-PC>
2012-04-23 21:22:53 +0200

    ContentHandler now starts handling the content, parsing the data and saving it to the database.
    Still have to put in the js calls when the javascript functions are done

--- lolApp/contentHandler.cpp
+++ lolApp/contentHandler.cpp
@@ -13,16 +13,64 @@
  */
 ContentHandler::ContentHandler(WebView* main) {
 	this->main = main;
+	this->newsReady = false;
+	this->competitiveready = false;
+	this->dataReady = false;
+	this->thereIsNeedToParse = true;
 
-	sourceCodeLoader = new SourceCodeLoader();
+	sourceCodeLoader = sourceCodeLoader->getInstance();
 	lolSourceParser = new LolSourceParser();
+	database = new DatabaseHandler();
+
+	if(thereIsNeedToParse) {
+		sourceCodeLoader->getDataFromUrl("http://euw.leagueoflegends.com/news");
+	}
+	else {
+		database->getNewsData();
+	}
 }
 
 /**
  * Function that executes when there is nothing else to do
  */
 void ContentHandler::idle() {
+	if(thereIsNeedToParse) {
+		//If all the data is not ready yet, go check what is not ready
+		if(!this->dataReady) {
+			//If news ain't ready yet, go make it ready :o)
+			if(!this->newsReady) {
+				if(sourceCodeLoader->dataReady()) {
+					char* data = sourceCodeLoader->getData();
+					if(lolSourceParser->parseMoreNews()) { // we parse once every cycle
+						NewsModel* news = lolSourceParser->parseNews(data);
+						database->addNews(news);
 
+						//TODO callJS function on the webview where we set the data to the handler
+
+					}
+					else { //means there is no more news to be parsed
+						this->newsReady = true;
+					}
+
+				}
+			}//if news is ready we will check if the competitive is ready
+			else if(!this->competitiveready) {
+				this->competitiveready = true;
+			}
+
+			//If all the data is ready, we set all data ready to true
+			if(this->competitiveready && this->newsReady) {
+				this->dataReady = true;
+			}
+		}
+	}
+	else {
+		if(!this->dataReady) {
+			if(database->newsReady()) {
+				char* news = database->getNewsData();
+			}
+		}
+	}
 }
 
 
