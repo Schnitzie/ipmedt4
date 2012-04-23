3d813ae69d9bb7c7f62d4ce65bf7e7be714ae65c
Douw3rd <Douw3rd@Douw3rd-Flaptop>
2012-04-23 20:12:47 +0200

    parser

---
+++ lolApp/newsModel.h
@@ -0,0 +1,23 @@
+/*
+ * lolSourceParserNews.h
+ *
+ *  Created on: 18 apr. 2012
+ *      Author: Douw3rd
+ */
+
+#ifndef NEWSMODEL_H_
+#define NEWSMODEL_H_
+
+class NewsModel {
+private:
+
+public:
+	NewsModel();
+	char* title;
+	char* content;
+	char* date;
+	char* author;
+};
+
+
+#endif /* LOLSOURCEPARSERNEWS_H_ */
